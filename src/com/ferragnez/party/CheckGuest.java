b284bc15bc06d88eb3a6bf9f26bb6af9ef537e30
lorenzosalti <lorenzosalti@gmail.com>
2025-06-25 14:50:14 +0200

    feat: guests array initialized

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,5 +1,13 @@
 package com.ferragnez.party;
 
-public class CheckGuest {
+import java.util.Scanner;
 
+public class CheckGuest {
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    String[] guests = { "Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi",
+        "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic" };
+  }
 }
