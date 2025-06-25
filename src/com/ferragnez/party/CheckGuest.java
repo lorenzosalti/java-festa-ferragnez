5baf8607809d194fddf9054f99732d9f97af2589
lorenzosalti <lorenzosalti@gmail.com>
2025-06-25 14:53:12 +0200

    feat: guest name input

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -6,8 +6,14 @@ public class CheckGuest {
   public static void main(String[] args) {
 
     Scanner sc = new Scanner(System.in);
+    String guestName;
 
     String[] guests = { "Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi",
         "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic" };
+
+    System.out.println("Inserire il proprio nome");
+    guestName = sc.nextLine();
+
+    System.out.println(guestName);
   }
 }
