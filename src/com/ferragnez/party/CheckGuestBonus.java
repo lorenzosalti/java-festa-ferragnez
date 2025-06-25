6ed477bea5492aa163f3ffcf5edf153da4c9194c
lorenzosalti <lorenzosalti@gmail.com>
2025-06-25 15:42:19 +0200

    feat: Bonus with while loop

---
+++ src/com/ferragnez/party/CheckGuestBonus.java
@@ -0,0 +1,35 @@
+package com.ferragnez.party;
+
+import java.util.Scanner;
+
+public class CheckGuestBonus {
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    String guestName;
+
+    String[] guests = { "Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi",
+        "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic" };
+
+    System.out.println("Inserire il proprio nome");
+    guestName = sc.nextLine().replaceAll("\\s+", "").toLowerCase();
+
+    int i = 0;
+    while (i < guests.length) {
+
+      String currentGuest = guests[i].replaceAll("\\s+", "").toLowerCase();
+
+      if (currentGuest.equals(guestName)) {
+        System.out.println("Ciao " + guests[i] + ", ti do il benvenuto a questa esclusiva festa a casa Ferragnez.");
+        break;
+      } else if (i == guests.length - 1) {
+        System.out.println("Mi spiace Coso, ma non sei sulla lista. Torna da TonyEffe.");
+      }
+
+      i++;
+
+    }
+
+    sc.close();
+  }
+}
